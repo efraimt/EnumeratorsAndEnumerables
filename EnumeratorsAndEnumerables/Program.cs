c678a435c730dedb7cecd4f95328ada0be4b6aa2
efraimt <efraimt@gmail.com>
2021-09-14 00:55:24 +0300

    Add project files.

---
+++ EnumeratorsAndEnumerables/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnumeratorsAndEnumerables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] vs = { 1, 2, 3 };
+            foreach (var item in vs)
+            {
+
+            }
+
+            List<int> vs1 = new List<int>(new int[] { 1, 2, 3 });
+            foreach (var item in vs1)
+            {
+
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i * 10);
+            }
+
+            //var myMofaOfSidra = new Sidra10To100();
+
+            //while (myMofaOfSidra.MoveNext())
+            //{
+            //    var item = myMofaOfSidra.Current;
+            //}
+
+
+            foreach (var item in new Sidra10To100())
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+
+
+    }
+}
---
+++ EnumeratorsAndEnumerables/SidraZeroTo100.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratorsAndEnumerables
+{
+
+    public class Sidra10To100 : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                yield return i * 10;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    ////public class Sidra10To100 : IEnumerable//<int>
+    //{
+    //    int i = 0;
+    //    public bool MoveNext()
+    //    {
+    //        if (i <= 10)
+    //        {
+    //            i++;
+    //            return true;
+    //        }
+    //        return false;
+    //    }
+
+    //    public void Reset()
+    //    {
+    //        i = 0;
+    //    }
+
+    //    public int Current { get { return i * 10; } }
+
+
+    //    //public IEnumerator<int> GetEnumerator()
+    //    //{
+
+    //    //}
+
+    //    //IEnumerator IEnumerable.GetEnumerator()
+    //    //{
+    //    //    throw new NotImplementedException();
+    //    //}
+    //}
+}
